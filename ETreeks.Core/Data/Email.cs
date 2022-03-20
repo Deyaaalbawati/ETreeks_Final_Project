8fb85bb4ef3265a9d4bb67b30f5fceb0f498304c
danaabdallah701@gmail.com <danaabdallah701@gmail.com>
2022-03-20 17:24:47 +0300

    exteranl api email

---
+++ ETreeks.Core/Data/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETreeks.Core.Data
+{
+   public class Email
+    {
+        public string EmailFrom { get; set; }
+        public string Password { get; set; }
+        public string EmailTo { get; set; }
+        public string textMsg { get; set; }
+    }
+}
